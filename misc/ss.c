6cfc7532826d44163e8d60e73a8cdd3877895867
zhaozhentao <qq344696734@gmail.com>
2025-02-03 09:34:46 +0800

    创建一个用于获取 socket 信息的 socket

--- misc/ss.c
+++ misc/ss.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include <sys/socket.h>
+#include <linux/netlink.h>
 
 int main(void) {
+    int fd;
+
+    /*
+     * obtaining information about sockets
+     * @see https://man7.org/linux/man-pages/man7/sock_diag.7.html
+     */
+    fd = socket(AF_NETLINK, SOCK_RAW, NETLINK_SOCK_DIAG);
+
+    if (fd < 0) {
+        perror("socket");
+        return 1;
+    }
 
     return 0;
 }
