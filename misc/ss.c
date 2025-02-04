d0bf108b8ee87a86666a6992a321a9b6b6c7e290
zhaozhentao <qq344696734@gmail.com>
2025-02-04 11:43:30 +0800

    显示连接 ip

--- misc/ss.c
+++ misc/ss.c
@@ -1,37 +1,40 @@
 #include <errno.h>
 #include <stdio.h>
-#include <string.h>
 #include <unistd.h>
 #include <sys/socket.h>
-#include <sys/un.h>
 #include <linux/netlink.h>
-#include <linux/rtnetlink.h>
 #include <linux/sock_diag.h>
-#include <linux/unix_diag.h>
+#include <linux/inet_diag.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <arpa/inet.h>
 
 static int send_query(int fd) {
     struct sockaddr_nl nladdr = {
             .nl_family = AF_NETLINK
     };
+
     struct {
         struct nlmsghdr nlh;
-        struct unix_diag_req udr;
+        struct inet_diag_req_v2 r;
     } req = {
             .nlh = {
                     .nlmsg_len = sizeof(req),
                     .nlmsg_type = SOCK_DIAG_BY_FAMILY,
                     .nlmsg_flags = NLM_F_REQUEST | NLM_F_DUMP
             },
-            .udr = {
-                    .sdiag_family = AF_UNIX,
-                    .udiag_states = -1,
-                    .udiag_show = UDIAG_SHOW_NAME | UDIAG_SHOW_PEER
+            .r = {
+                    .sdiag_family = AF_INET,
+                    .sdiag_protocol = IPPROTO_TCP,
+                    .idiag_states = (1 << TCP_ESTABLISHED),
             }
     };
+
     struct iovec iov = {
             .iov_base = &req,
             .iov_len = sizeof(req)
     };
+
     struct msghdr msg = {
             .msg_name = &nladdr,
             .msg_namelen = sizeof(nladdr),
@@ -48,66 +51,44 @@ static int send_query(int fd) {
             return -1;
         }
 
+        printf("send success\n");
         return 0;
     }
 }
 
-int print_diag(const struct unix_diag_msg *diag, unsigned int len) {
+const char *format_host(int af, const void *addr) {
+    static char buf[256];
+
+    return inet_ntop(af, addr, buf, 256);
+}
+
+int print_diag(const struct inet_diag_msg *diag, unsigned int len) {
     if (len < NLMSG_LENGTH(sizeof(*diag))) {
         fputs("short response\n", stderr);
         return -1;
     }
-    if (diag->udiag_family != AF_UNIX) {
-        fprintf(stderr, "unexpected family %u\n", diag->udiag_family);
+
+    if (diag->idiag_family != AF_INET) {
+        fprintf(stderr, "unexpected family %u\n", diag->idiag_family);
         return -1;
     }
 
-    unsigned int rta_len = len - NLMSG_LENGTH(sizeof(*diag));
-    unsigned int peer = 0;
-    size_t path_len = 0;
-    char path[sizeof(((struct sockaddr_un *) 0)->sun_path) + 1];
+    const char * local = format_host(AF_INET, diag->id.idiag_src);
+    const char * peer = format_host(AF_INET, diag->id.idiag_dst);
 
-    for (struct rtattr *attr = (struct rtattr *) (diag + 1);
-         RTA_OK(attr, rta_len); attr = RTA_NEXT(attr, rta_len)) {
-        switch (attr->rta_type) {
-            case UNIX_DIAG_NAME:
-                if (!path_len) {
-                    path_len = RTA_PAYLOAD(attr);
-                    if (path_len > sizeof(path) - 1)
-                        path_len = sizeof(path) - 1;
-                    memcpy(path, RTA_DATA(attr), path_len);
-                    path[path_len] = '\0';
-                }
-                break;
+    printf("local %s peer %s state %d\n", local, peer, diag->idiag_state);
 
-            case UNIX_DIAG_PEER:
-                if (RTA_PAYLOAD(attr) >= sizeof(peer))
-                    peer = *(unsigned int *) RTA_DATA(attr);
-                break;
-        }
-    }
-
-    printf("inode=%u", diag->udiag_ino);
-
-    if (peer)
-        printf(", peer=%u", peer);
-
-    if (path_len)
-        printf(", name=%s%s", *path ? "" : "@",
-               *path ? path : path + 1);
-
-    putchar('\n');
     return 0;
 }
 
 int receive_responses(int fd) {
+    int flags = 0;
     long buf[8192 / sizeof(long)];
     struct sockaddr_nl nladdr;
     struct iovec iov = {
             .iov_base = buf,
             .iov_len = sizeof(buf)
     };
-    int flags = 0;
 
     for (;;) {
         struct msghdr msg = {
@@ -126,6 +107,7 @@ int receive_responses(int fd) {
             perror("recvmsg");
             return -1;
         }
+
         if (ret == 0)
             return 0;
 
@@ -159,8 +141,7 @@ int receive_responses(int fd) {
             }
 
             if (h->nlmsg_type != SOCK_DIAG_BY_FAMILY) {
-                fprintf(stderr, "unexpected nlmsg_type %u\n",
-                        (unsigned) h->nlmsg_type);
+                fprintf(stderr, "unexpected nlmsg_type %u\n", (unsigned) h->nlmsg_type);
                 return -1;
             }
 
@@ -182,8 +163,11 @@ int main(void) {
         return 1;
     }
 
-    int ret = send_query(fd) || receive_responses(fd);
+    send_query(fd);
+
+    receive_responses(fd);
 
     close(fd);
-    return ret;
+
+    return 0;
 }
